ace65c2cc1db919728f0d32c44b9b94044ccc34e
Abra.Z <zhangxiangyun@ixiangyun.me>
2017-11-02 10:12:35 +0800

    delete weixin config from cfg.inc.php

--- config/cfg.inc.php
+++ config/cfg.inc.php
@@ -10,18 +10,6 @@ define("APP_NAME", "总裁意见箱");
 define("DATA_DIR", __DIR__ . '/..');
 define( "RECORD_LIMIT_PER_PAGE", 10 );
 
-define("SERVER_IP", "");
-// 微信相关设置
-define("APPID" , "weixin appid");
-define("APP_SECRET", "weixin app secret");
-define("TOKEN", "weixin token");
-define("MCHID", "wxpay mchid");
-define("PayKey", "wxpay paykey");
-define("REDIRECT_URI", 'project url');
-define("SCOPE", "snsapi_userinfo");
-define('WXPAY_SSLCERT_PATH', 'ssl cert path');
-define('WXPAY_SSLKEY_PATH', 'ssl key path');
-
 // 意见相关设置
 $TIP_STATUS = [
 	0	=>	"未处理",
@@ -30,7 +18,7 @@ $TIP_STATUS = [
 ];
 
 // 用户角色
-define("ZC_USER", "0");
-define("ZC_SUPER_ADMIN", "1");
-define("ZC_ADMIN", "2");
+define("ZC_USER", "0");	//普通用户
+define("ZC_SUPER_ADMIN", "1");	//超级管理员，可发微信红包
+define("ZC_ADMIN", "2");	//普通管理员，可操作意见
 ?>
